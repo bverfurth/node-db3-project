695613669dc138fa8c08c3957be3dfb04300199e
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-09-08 17:53:44 -0400

    fixed the middleware faults

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,44 +1,48 @@
+const db = require("./../../data/db-config");
+
 const checkSchemeId = async (req, res, next) => {
-  const { scheme_id } = req.params;
   try {
-    await db("schemes").where({ scheme_id }).first();
-    req.schemeSearch = { scheme_id };
+    const existingID = await db("schemes")
+      .where("scheme_id", req.params.scheme_id)
+      .first();
+    if (!existingID) {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`,
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
     next();
-  } catch {
-    res
-      .status(404)
-      .json({ message: `scheme with scheme ID ${scheme_id} not found` });
   }
 };
 
 const validateScheme = (req, res, next) => {
-  const newScheme = req.body;
-
-  if (
-    !newScheme.scheme_name ||
-    newScheme.scheme_name.length === 0 ||
-    typeof newScheme.scheme_name !== "string"
-  ) {
-    res.status(400).json({ message: "invalid scheme_name" });
+  const { scheme_name } = req.body;
+  if (!scheme_name || typeof scheme_name !== "string" || !scheme_name.trim()) {
+    next({
+      status: 400,
+      message: "invalid scheme_name",
+    });
   } else {
     next();
   }
 };
 
 const validateStep = (req, res, next) => {
-  const newStep = req.body;
-  console.log("This should be the step instructions,", newStep);
+  const { instructions, step_number } = req.body;
   if (
-    !newStep.instructions ||
-    newStep.instructions.length === 0 ||
-    typeof newStep.instructions !== "string"
+    !instructions ||
+    typeof instructions !== "string" ||
+    !instructions.trim() ||
+    typeof step_number !== "number" ||
+    step_number < 1
   ) {
-    res.status(400).json({ message: "missing instruction" });
-  } else if (
-    typeof newStep.step_number !== "number" ||
-    newStep.step_number < 1
-  ) {
-    res.status(400).json({ message: "bad step_number" });
+    next({
+      status: 400,
+      message: "invalid step",
+    });
   } else {
     next();
   }
