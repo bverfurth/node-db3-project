5a80a1f343572e71291417117c92096505f4cbeb
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-09-08 14:37:29 -0400

    set up middleware functions

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,42 +1,51 @@
-/*
-  If `scheme_id` does not exist in the database:
-
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
+const checkSchemeId = async (req, res, next) => {
+  const { scheme_id } = req.params;
+  try {
+    await db("schemes").where({ scheme_id }).first();
+    req.schemeSearch = { scheme_id };
+    next();
+  } catch {
+    res
+      .status(404)
+      .json({ message: `scheme with scheme ID ${scheme_id} not found` });
   }
-*/
-const checkSchemeId = (req, res, next) => {
+};
 
-}
-
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
+  const newScheme = req.body;
 
-}
-
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
+  if (
+    !newScheme.scheme_name ||
+    newScheme.scheme_name.length === 0 ||
+    typeof newScheme.scheme_name !== "string"
+  ) {
+    res.status(400).json({ message: "invalid scheme_name" });
+  } else {
+    next();
   }
-*/
-const validateStep = (req, res, next) => {
+};
 
-}
+const validateStep = (req, res, next) => {
+  const newStep = req.body;
+  console.log("This should be the step instructions,", newStep);
+  if (
+    !newStep.instructions ||
+    newStep.instructions.length === 0 ||
+    typeof newStep.instructions !== "string"
+  ) {
+    res.status(400).json({ message: "missing instruction" });
+  } else if (
+    typeof newStep.step_number !== "number" ||
+    newStep.step_number < 1
+  ) {
+    res.status(400).json({ message: "bad step_number" });
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
